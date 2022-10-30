4e4d2b7d2ac132fa030be74b2fa7f3a89468939d
Vadim36 <strannik.vad@gmail.com>
2022-10-30 10:36:44 +0200

    less 3-3-all +

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # Instalation
 
+- composer install
 - php artisan shop:install
-
+- make .env. and .env.testing from .env.example !!!IMPORTANT
 
 
 
---
+++ tests/Feature/App/Http/Controllers/AuthControllerTest.php
@@ -0,0 +1,130 @@
+<?php
+
+namespace Tests\Feature\App\Http\Controllers;
+
+use App\Http\Controllers\AuthController;
+use App\Http\Requests\SignInFormRequest;
+use App\Http\Requests\SignUpFormRequest;
+use App\Listeners\SendEmailNewUserListener;
+use App\Models\User;
+use App\Notifications\NewUserNotification;
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Event;
+use Illuminate\Support\Facades\Notification;
+use Tests\RequestFactories\SignUpFormRequestFactory;
+use Tests\TestCase;
+
+class AuthControllerTest extends TestCase
+{
+    use RefreshDatabase;
+
+
+    public function test_it_login_page_success()
+    {
+        $this->get(action([AuthController::class, 'index']))
+            ->assertOk()
+            ->assertSee('Вход в аккаунт')
+            ->assertViewIs('auth.index');
+    }
+
+
+    public function test_it_sign_up_page_success()
+    {
+        $this->get(action([AuthController::class, 'signUp']))
+            ->assertOk()
+            ->assertSee('Регистрация')
+            ->assertViewIs('auth.sign-up');
+    }
+
+
+    public function test_it_forgot_page_success()
+    {
+        $this->get(action([AuthController::class, 'forgot']))
+            ->assertOk()
+            ->assertSee('Забыли пароль?')
+            ->assertViewIs('auth.forgot-password');
+    }
+
+
+    public function test_it_sign_in_success()
+    {
+        $password = '123456789';
+
+        $user = User::factory()->create([
+            'email' => '12345@gmail.com',
+            'password' => bcrypt($password),
+        ]);
+
+        $request = SignInFormRequest::factory()->create([
+            'email' => $user->email,
+            'password' => $password,
+        ]);
+
+        $response = $this->post(action([AuthController::class, 'signIn'], $request));
+
+        $response->assertValid()
+            ->assertRedirect(route('home'));
+
+        $this->assertAuthenticatedAs($user);
+    }
+
+
+    public function test_it_store_success()
+    {
+        Notification::fake();
+        Event::fake();
+
+        $request = SignUpFormRequest::factory()->create([
+            'email' => '12345678@gmail.com',
+            'password' => '12345678',
+            'password_confirmation' => '12345678'
+        ]);
+
+        $this->assertDatabaseMissing('users', ['email' => '12345678@gmail.com']);
+
+        $response = $this->post(
+            action([AuthController::class, 'store']),
+            $request
+        );
+
+        $response->assertValid();
+
+        $this->assertDatabaseHas('users', ['email' => '12345678@gmail.com']);
+
+        $user = User::query()
+            ->where('email', $request['email'])
+            ->first();
+
+        Event::assertDispatched(Registered::class);
+        Event::assertListening(Registered::class, SendEmailNewUserListener::class);
+
+        $event = new Registered($user);
+        $listener = new SendEmailNewUserListener();
+        $listener->handle($event);
+
+        Notification::assertSentTo($user, NewUserNotification::class);
+
+        $this->assertAuthenticatedAs($user);
+
+        $response->assertRedirect(route('home'));
+    }
+
+
+    public function test_it_logout_success()
+    {
+
+        $user = User::factory()->create([
+            'email' => '12345@gmail.com',
+        ]);
+
+        $this->actingAs($user)->delete(action([AuthController::class, 'logOut']));
+
+        $this->assertGuest();
+    }
+
+
+    //todo ДЗ) - дописать тесты на методы что ещё не сделаны.
+
+
+}
