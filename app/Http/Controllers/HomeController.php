dcce91579916735f91b827ecc59a5d900501f79e
Vadim36 <strannik.vad@gmail.com>
2022-11-14 16:23:08 +0200

    начало 5 этапа. откуда столько новых файлов в 1 уроке?

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -11,7 +11,6 @@ class HomeController extends Controller
 {
     public function __invoke()
     {
-    \Debugbar::addMessage('HomeController', __FILE__ . ':' . __LINE__);
 
         $categories = CategoryViewModel::make()
             ->homePage();
