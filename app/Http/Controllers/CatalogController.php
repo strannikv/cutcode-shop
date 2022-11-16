941061292426e5d4081616066df959b3dd0b3e4d
Vadim36 <strannik.vad@gmail.com>
2022-11-16 20:17:27 +0200

    5-1...

--- app/Http/Controllers/CatalogController.php
+++ app/Http/Controllers/CatalogController.php
@@ -12,17 +12,20 @@ class CatalogController extends Controller
     public function __invoke(?Category $category)
     {
         $brands = Brand::query()
+            ->select('id', 'title')
             ->has('products')
             ->get();
 
         $categories = Category::query()
+            ->select('id', 'title', 'slug')
             ->has('products')
             ->get();
 
         $products = Product::query()
+            ->select('id', 'title', 'slug', 'price', 'thumbnail')
             ->paginate(6);
 
-        return view('index', compact(
+        return view('catalog.index', compact(
             'categories',
             'category',
             'products',
--- resources/views/catalog/shared/category.blade.php
+++ resources/views/catalog/shared/category.blade.php
@@ -1,5 +1,5 @@
 {{--<a href="{{ route('catalog') }}"--}}
-<a href="{{ route('catalog', $item->slug) }}"
+<a href="{{ route('catalog', $item) }}"
    class="p-3 sm:p-4 2xl:p-6 rounded-xl bg-card hover:bg-pink text-xxs sm:text-xs lg:text-sm text-white font-semibold">
    {{ $item->title }}
 </a>
