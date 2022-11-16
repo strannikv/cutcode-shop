19e6954aea4a81dd163121f8af16b6c6c8a5a7bd
Vadim36 <strannik.vad@gmail.com>
2022-11-16 19:52:27 +0200

    5-1...

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use App\Models\Product;
 use Database\Factories\BrandFactory;
+use Database\Factories\CategoryFactory;
 use Domain\Catalog\Models\Brand;
 use Domain\Catalog\Models\Category;
 use Illuminate\Database\Seeder;
@@ -21,7 +22,7 @@ class DatabaseSeeder extends Seeder
        BrandFactory::new()->count(20)->create();
 
        Product::factory(20)
-           ->has(BrandFactory::new()->count(1-3))
+           ->has(Category::factory(rand(1,3)))
            ->create();
     }
 }
