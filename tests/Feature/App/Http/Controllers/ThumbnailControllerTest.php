715c1af3cbbf367111822ddd92ea55fdfbe44caa
Vadim36 <strannik.vad@gmail.com>
2022-11-19 13:06:53 +0200

    5-3 ...

---
+++ tests/Feature/App/Http/Controllers/ThumbnailControllerTest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Tests\Feature\App\Http\Controllers;
+
+use App\Models\Product;
+use Database\Factories\ProductFactory;
+use Domain\Catalog\Models\Brand;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Storage;
+use Tests\TestCase;
+
+class ThumbnailControllerTest extends TestCase
+{
+    use RefreshDatabase;
+
+
+    public function test_it_success_response()
+    {
+        $size = '500x500';
+        $method = 'resize';
+        $storage = Storage::disk('images');
+
+        config()->set('thumbnail', ['allowed_sizes' => [$size]]);
+
+        Brand::factory()->create();
+        $product = ProductFactory::new()->create();
+
+        $response = $this->get($product->makeThumbnail($size, $method));
+
+        $response->assertOk();
+
+
+        $storage->assertExists(
+            "products/$method/$size/" . File::basename($product->thumbnail)
+        );
+
+
+    }
+
+
+
+
+}
